d569a112f0f1dff9606906825b09b798670ceec1
catrat <loadedcoal@gmail.com>
2024-05-23 14:55:48 -0400

    second

--- main.py
+++ main.py
@@ -1,34 +1,102 @@
 import pprint
-
 import requests
 import os
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv, dotenv_values
+import datetime
 
-dotenv_path = find_dotenv()
+load_dotenv()
 
 AV_url = os.getenv('alpha_vantage_url')
-AV_api_key = os.getenv('alphaV_api_key')
+AV_api_key = os.getenv('alphaV_api_key_second')
 
 NAPI_url = os.getenv('newsapi_url')
 NAPI_api_key = os.getenv('newsapi_key')
 
-STOCK = "TSLA"
+STOCK = "ELF"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines"
 
-stock_params = {
-    'function': 'Time_Series_Daily',
-    'symbol': 'IBM',
-    'apikey': AV_api_key
-}
+# Function to call the API and get data to work with
+def call_stock_api(stock, api_key, endpoint):
 
-stock_call = requests.get(STOCK_ENDPOINT, params=stock_params)
-stock_call.raise_for_status()
-dataS = stock_call.json()
+    stock_params = {
+        'function': 'Time_Series_Daily',
+        'symbol': stock,
+        'apikey': api_key,
 
-pprint.pprint(dataS)
+    }
+
+    stock_call = requests.get(endpoint, params=stock_params)
+    stock_call.raise_for_status()
+    dataS = stock_call.json()
+
+    print(dataS)
+
+    one_day_before_yesterday = round(float(dataS['Time Series (Daily)']['2024-05-21']['4. close']))
+    yesterday = round(float(dataS['Time Series (Daily)']['2024-05-22']['4. close']))
+
+    return yesterday, one_day_before_yesterday
+
+# Function to find if the price has made a sufficient movement
+def find_sufficient_movements(day, day_before):
+    a = .05
+    b = a * day_before
+    lower = day_before - b
+    higher = day_before + b
+
+    if day > day_before and day > higher:
+        diff = day - day_before
+        direction = 'up'
+        return day, diff, direction
+    elif day < day_before and day < lower:
+        diff = day_before - day
+        direction = 'down'
+        return day, diff, direction
+    else:
+        diff = 'stable'
+        direction = 'stable'
+        return day, diff, direction
+
+
+# yesterday, day_before = call_stock_api(STOCK, AV_api_key, STOCK_ENDPOINT)
+#
+# days_price, difference, direction = find_sufficient_movements(yesterday, day_before)
+
+
+# news start
+
+today = datetime.date.today()
+old = today - datetime.timedelta(days=10)
+
+def find_stories(stock):
+    news_params = {
+        'q': 'Tesla',
+        'pageSize': 3,
+        'sortBy': 'popularity',
+        'apikey': NAPI_api_key
+    }
+
+    grab_news = requests.get(NEWS_ENDPOINT, params=news_params)
+    grab_news.raise_for_status()
+    dataN = grab_news.json()
+
+    article_one = (f'Title: {dataN['articles'][0]['title']}\nSource: {dataN['articles'][0]['source']['name']}\nLink: {dataN['articles'][0]['url']}\n')
+
+    article_two = (f'Title: {dataN['articles'][1]['title']}\nSource: {dataN['articles'][1]['source']['name']}\nLink: {dataN['articles'][1]['url']}\n')
+
+    article_three = (f'Title: {dataN['articles'][2]['title']}\nSource: {dataN['articles'][2]['source']['name']}\nLink: {dataN['articles'][2]['url']}\n')
+
+    return article_one, article_two, article_three
+
+a, b, c = find_stories('TSLA')
+
+
+
+
+
+# pprint.pp(article_one)
 
 
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
