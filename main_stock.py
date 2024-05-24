548fd14e835aa54e013b047525ca1abe72919237
catrat <loadedcoal@gmail.com>
2024-05-23 21:13:29 -0400

    Third, whole thing should be functional at this point but I am out of API calls currently working around that.

--- main_stock.py
+++ main_stock.py
@@ -3,8 +3,15 @@ import requests
 import os
 from dotenv import load_dotenv, dotenv_values
 import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 load_dotenv()
+
+# Accounts and keys
 
 AV_url = os.getenv('alpha_vantage_url')
 AV_api_key = os.getenv('alphaV_api_key_second')
@@ -12,8 +19,16 @@ AV_api_key = os.getenv('alphaV_api_key_second')
 NAPI_url = os.getenv('newsapi_url')
 NAPI_api_key = os.getenv('newsapi_key')
 
-STOCK = "ELF"
+gmail = os.getenv('burner_gmail')
+gmail_api_key = os.getenv('gmail_app_pass')
+
+# Constants
+
+STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+today = datetime.date.today()
+old = today - datetime.timedelta(days=10)
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines"
@@ -59,16 +74,21 @@ def find_sufficient_movements(day, day_before):
         direction = 'stable'
         return day, diff, direction
 
+# Function for to check percentage of move and return with an arrow
+def find_move_percent(day, difference, direction):
+    percentage = (int(difference) / int(day)) * 100
+    arrow = ''
+    if direction == 'up':
+        arrow = '\u2191'
+    else:
+        arrow = '\u2193'
 
-# yesterday, day_before = call_stock_api(STOCK, AV_api_key, STOCK_ENDPOINT)
-#
-# days_price, difference, direction = find_sufficient_movements(yesterday, day_before)
+    return f'{arrow} {percentage}%'
 
 
-# news start
+price_yesterday, price_day_before = call_stock_api(STOCK, AV_api_key, STOCK_ENDPOINT)
 
-today = datetime.date.today()
-old = today - datetime.timedelta(days=10)
+days_price, difference, direction = find_sufficient_movements(price_yesterday, price_day_before)
 
 def find_stories(stock):
     news_params = {
@@ -90,44 +110,25 @@ def find_stories(stock):
 
     return article_one, article_two, article_three
 
-a, b, c = find_stories('TSLA')
+art_one, art_two, art_three = find_stories('TSLA')
+
+move_percent = find_move_percent(days_price, difference, direction)
+
+message = MIMEMultipart()
+message['From'] = gmail
+message['To'] = 'loadedcoal@gmail.com'
+message['Subject'] = f'{STOCK} is {move_percent} today!'
+
+body = f'Subject: {STOCK} is {move_percent} today\n\nHello,\n{STOCK} is {move_percent} today! Here are the latest stories:\n\n{art_one}\n{art_two}\n{art_three}'
+message.attach(MIMEText(body, 'plain', 'utf-8'))
+
+connection = smtplib.SMTP('smtp.gmail.com', 587)
+connection.starttls()
+connection.login(user=gmail, password=gmail_api_key)
+connection.sendmail(gmail, 'loadedcoal@gmail.com', message.as_string())
+
+print('Your email was sent!')
 
 
 
-
-
-# pprint.pp(article_one)
-
-
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price.
-
-
-
-
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
-
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
